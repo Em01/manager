35c63186db9ca1d351fa28051a782bee8ca16100
Em01 <emma_williams1@hotmail.co.uk>
2017-02-05 19:36:50 +0000

    wip: auth error

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Text } from 'react-native'
 import { connect } from 'react-redux'
 import { emailChanged, passwordChanged, loginUser } from '../actions'
 import { Card, CardSection, Input, Button } from './common'
@@ -39,6 +40,10 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.props.error}
+        </Text>
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -50,11 +55,20 @@ class LoginForm extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  const { email, password } = state.auth
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+}
+
+const mapStateToProps = ({ auth }) => {
+  const { email, password, error } = auth
   return {
-    email: email,
-    password: password
+    email,
+    password,
+    error
   }
 }
 
