99517302dda0b4b5f106c0fdc16a14af5d36367f
Em01 <emma_williams1@hotmail.co.uk>
2017-02-05 18:38:48 +0000

    add redux thunk and login action creator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import firebase from 'firebase'
 import reducers from './reducers'
 import LoginForm from './components/LoginForm'
-
-// const store = createStore(reducers)
+import ReduxThunk from 'redux-thunk'
 
 class App extends Component {
   componentWillMount() {
@@ -21,8 +19,10 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
+
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     )
