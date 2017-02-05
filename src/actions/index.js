a142a021d50ce5496069e349510200d086236801
Em01 <emma_williams1@hotmail.co.uk>
2017-02-05 17:49:16 +0000

    Fix reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 import { EMAIL_CHANGED } from './types'
 
 export const emailChanged = (text) => {
-  console.log(text)
   return {
     type: EMAIL_CHANGED,
     payload: text
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,12 +1,14 @@
-import { EMAIL_CHANGED } from '../actions/types'
+import { EMAIL_CHANGED } from '../actions/types';
 
-const INITIAL_STATE = { email: '' }
+const INITIAL_STATE = { email: ''};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'EMAIL_CHANGED':
-      return { ...state, email: action.payload };
+    case EMAIL_CHANGED:
+    console.log('ec')
+     return {...state, email: action.payload};
     default:
+    console.log('def')
       return state;
   }
-};
+}
