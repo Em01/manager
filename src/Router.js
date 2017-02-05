ca5326b24f52178ddeeeac9fb31c0de81985941a
Em01 <emma_williams1@hotmail.co.uk>
2017-02-05 21:34:19 +0000

    New Scene

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,33 @@
 import React from 'react'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
   //with nesting you cannot go straight to a nested scene
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key="auth">
-        <Scene key="login" component={LoginForm} title="Please Login" />
+        <Scene
+          key="login"
+          component={LoginForm}
+          title="Please Login"
+        />
       </Scene>
 
       <Scene key="main">
         <Scene
-          onRight={() => console.log('right')}
-          rightTitle="Add"
           key="employeeList"
           component={EmployeeList}
           title="Employees"
-
+          onRight={() => Actions.employeeCreate()}
+          rightTitle="Add"
+        />
+        <Scene
+          key="employeeCreate"
+          component={EmployeeCreate}
+          title="Create Employee"
         />
       </Scene>
     </Router>
