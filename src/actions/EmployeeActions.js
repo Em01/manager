a4482e363877f71f3863460bbedf7973b5a73812
Em01 <emma_williams1@hotmail.co.uk>
2017-02-18 16:05:10 +0000

    Clear form atts

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,11 @@
 import firebase from 'firebase'
 import { Actions } from 'react-native-router-flux'
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types'
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS,
+  EMPLOYEE_SAVE_SUCCESS
+ } from './types'
 
 export const employeeUpdate = ({ prop, value}) => {
   return {
@@ -35,9 +40,12 @@ export const employeesFetch = () => {
 export const employeeSave = ({ name, phone, shift, uid }) => {
   const { currentUser } = firebase.auth()
 
-  return () => {
+  return (dispatch) => {
     firebase.database.ref(`/users/${currentUser.uid}/employees/${uid}`)
       .set({ name, phone, shift })
-      .then(() => console.log('saved'))
+      .then(() => {
+        dispatch({ type: EMPLOYEE_SAVE_SUCCESS })
+        Actions.employeeList({ type: 'reset'})
+      })
   }
 }
