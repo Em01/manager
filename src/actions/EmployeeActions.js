32d641119ad886580903699e74d5fd6b0ef392c3
Em01 <emma_williams1@hotmail.co.uk>
2017-02-19 11:55:07 +0000

    delete action

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -4,7 +4,8 @@ import {
   EMPLOYEE_UPDATE,
   EMPLOYEE_CREATE,
   EMPLOYEES_FETCH_SUCCESS,
-  EMPLOYEE_SAVE_SUCCESS
+  EMPLOYEE_SAVE_SUCCESS,
+  EMPLOYEE_DELETE
  } from './types'
 
 export const employeeUpdate = ({ prop, value}) => {
@@ -47,5 +48,17 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
         dispatch({ type: EMPLOYEE_SAVE_SUCCESS })
         Actions.employeeList({ type: 'reset'})
       })
+  }
+}
+
+export const employeeDelete = ({ uid }) => {
+  const { currentUser } = firebase.auth()
+
+  return () => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+    .remove()
+    .then(() => {
+      Actions.employeeList({ type: 'reset' })
+    })
   }
 }
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 import Communications from 'react-native-communications'
 import { connect } from 'react-redux'
 import EmployeeForm from './EmployeeForm'
-import { employeeUpdate, employeeSave } from '../actions'
-import { Card, CardSection, Button } from './common'
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions'
+import { Card, CardSection, Button, Confirm } from './common'
 
 class EmployeeEdit extends Component {
+  state = { showModal: false }
+
   componentWillMount() {
     _.each(this.props.employee, (value, prop) => {
       this.props.employeeUpdate({ prop, value })
@@ -24,6 +26,16 @@ class EmployeeEdit extends Component {
     Communications.text(phone, `Your upcoming shift is on ${shift}`)
   }
 
+  onAccept () {
+    const { uid } = this.props.employee
+
+    this.props.employeeDelete({ uid })
+  }
+
+  onDecline () {
+    this.setState({ showModal: false })
+  }
+
   render() {
     return (
       <Card>
@@ -35,10 +47,24 @@ class EmployeeEdit extends Component {
         </CardSection>
 
         <CardSection>
-          <Button onPress={this.onTexxtPress.bind(this)}>
-            Save Changes
+          <Button onPress={this.onTextPress.bind(this)}>
+            Text Schedule
           </Button>
         </CardSection>
+
+        <CardSection>
+          <Button onPress={() => this.setState({ showModal: !this.state.showModal})}>
+            Fire Employee
+          </Button>
+        </CardSection>
+
+        <Confirm
+          visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
+        >
+          Are you sure you want to delete this?
+        </Confirm>
       </Card>
     )
   }
@@ -49,5 +75,5 @@ const mapStateToProps = (state) => {
   return { name, phone, shift}
 }
 export default connect(mapStateToProps,
-  {employeeUpdate, employeeSave
+  {employeeUpdate, employeeSave, employeeDelete
 })(EmployeeEdit)
